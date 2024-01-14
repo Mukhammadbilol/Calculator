78e28c0510cbfc15b97a71896266351d4c73de8b
Mukhammadbilol <muhammadbilolumaroff@gmail.com>
2024-01-14 15:49:00 +0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Welcome to calculator! Enter values.");
+
+Console.Write("First number: ");
+string firstValue = Console.ReadLine();
+Console.Write("Second number: ");
+string secondValue = Console.ReadLine();
+
+Console.WriteLine("Converting values in progress...");
+decimal firstNumber = Convert.ToDecimal(firstValue);
+decimal secondNumber = Convert.ToDecimal(secondValue);
+
+Console.WriteLine("Calculating results..."); 
+Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
